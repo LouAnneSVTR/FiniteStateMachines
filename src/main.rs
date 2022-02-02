6e7f7bec9e15de9473b8c10b6d08f0f58bb358ac
E185574L <lou-anne.sauvetre@etu.univ-nantes.fr>
2022-02-02 16:55:37 +0100

    add state and main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,58 @@
-struct FST;
+use std::fmt;
+use std::fmt::Formatter;
+
+pub struct Blue;
+
+// The `derive` attribute automatically creates the implementation
+// requiPink to make this `struct` printable with `fmt::Debug`.
+#[derive(Debug)]
+pub struct Orange;
+
+#[derive(Debug)]
+pub struct Pink;
+
+#[derive(Debug)]
+struct State<S> {
+    _inner: S
+}
+
+impl fmt::Debug for Blue {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Blue")
+            .finish()
+    }
+}
+
+impl State<Blue> {
+    pub fn new() -> State<Blue> {
+        State { _inner: Blue{} }
+    }
+}
+
+impl State<Blue> {
+    pub fn next(self) -> State<Orange> {
+        State { _inner: Orange {} }
+    }
+}
+
+
+impl State<Orange> {
+    pub fn next(self) -> State<Pink> {
+        State { _inner: Pink {} }
+    }
+}
+
+impl State<Pink> {
+    pub fn next(self) -> State<Blue> {
+        State { _inner: Blue {} }
+    }
+}
 
 fn main() {
-    //Creation of the finite state machine
-    let finiteStateMachine = FSM;
+    let state = State::new(); // Blue
+    let state = state.next(); // Orange
+    let state = state.next(); // Pink
+    let state = state.next(); // Blue
+    dbg!(state);
 }
+
